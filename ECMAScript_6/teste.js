1b21e73a66f372ac0b32be9649c6ea477fb9e75d
Carlos Roberto <carlosrjhoe@gmail.com>
2022-04-22 12:47:35 -0300

    continuamos o teste

---
+++ ECMAScript_6/teste.js
@@ -0,0 +1,12 @@
+// var nomes = ["Carlos", "Mayara", "Neto", "Luna"];
+// var sobreNomes = ["Roberto", "Ramos", "Conceição", "Conceição"];
+// nomes.forEach(function(nome){
+//   console.log(`Nome: ${nome}`)
+// });
+
+var canais = ["Globo",	"Sbt",	"Record"];
+canais.forEach(function(canal){
+  canais.push("RedeTv");
+  console.log(canal);
+});
+console.log(canais)
