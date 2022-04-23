13f40cbdbe3d9898ca00e8bf9416e016332a578a
Carlos Roberto <carlosrjhoe@gmail.com>
2022-04-22 22:33:58 -0300

    Melhorando exercicios: ForEach e Map

--- ECMAScript_6/forEach.js
+++ ECMAScript_6/forEach.js
@@ -1,4 +1,11 @@
 var nomes = ["Carlos", "Mayara", "Neto", "Luna"];
+
+// Primeiro,	fazemos	da	forma	convencional:
+for(var i = 0; i < nomes.length; i++){
+  console.log(nomes[i]);
+};
+
+// utilizando	o FOREACH:
 nomes.forEach(function(nome){
   console.log(`Nome: ${nome}`)
 });
--- ECMAScript_6/map.js
+++ ECMAScript_6/map.js
@@ -1,8 +1,14 @@
 var numeros = [1, 2, 3, 4, 5];
+var dobro = [];
+// Primeiro,	fazemos	da	forma	convencional:
+for(var i = 1; i < numeros.length + 1; i++){
+  dobro.push(i * 2)
+};
 
+// utilizando	o MAP:
 var dobro = numeros.map(function(numero){
   return numero * 2;
-})
+});
 
 console.log(numeros);
 console.log(dobro);
