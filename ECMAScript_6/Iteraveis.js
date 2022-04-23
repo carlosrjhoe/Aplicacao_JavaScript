737accc87ad82399677c849db32d3f6ae26c00ac
Carlos Roberto <carlosrjhoe@gmail.com>
2022-04-23 17:49:12 -0300

    Exemplo for of

--- ECMAScript_6/Iteraveis.js
+++ ECMAScript_6/Iteraveis.js
@@ -1,16 +1,25 @@
-function* iteraveis(){
-  yield "Olá 1";
-  yield "Olá 2";
-  yield "Olá 3";
-  yield "Olá 4";
-  console.log("Primeiro console");
-  console.log("Segundo console");
-  console.log("Terceiro console");
-  console.log("Quarto console");
-}
-let iterador = iteraveis();
+// function* iteraveis(){
+//   yield "Olá 1";
+//   yield "Olá 2";
+//   yield "Olá 3";
+//   yield "Olá 4";
+//   console.log("Primeiro console");
+//   console.log("Segundo console");
+//   console.log("Terceiro console");
+//   console.log("Quarto console");
+// }
+// let iterador = iteraveis();
 
-console.log(iterador.next());
-console.log(iterador.next());
-console.log(iterador.next());
-console.log(iterador.next());
+// console.log(iterador.next());
+// console.log(iterador.next());
+// console.log(iterador.next());
+// console.log(iterador.next());
+
+var bruxos = ["Carlos", "Mayara", "Neto", "Luna"];
+
+var iteradorBruxos = bruxos[Symbol.iterator]();
+
+console.log(iteradorBruxos.next());
+console.log(iteradorBruxos.next());
+console.log(iteradorBruxos.next());
+console.log(iteradorBruxos.next());
---
+++ ECMAScript_6/for_of.js
@@ -0,0 +1,14 @@
+var numeros = [36, 37, 6, 4];
+
+for(var numero of numeros){
+  console.log(numero);
+};
+
+// var perfilDoFace = {
+//   nome: "Carlos",
+//   idade: 36
+// };
+
+// for(dado of perfilDoFace){
+//   console.log(dado);
+// };
