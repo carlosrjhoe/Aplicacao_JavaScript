81962bc1105bc47a309ef7c8b9d4a59bfed99f47
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-06 22:16:14 -0300

    Modulos

---
+++ JavaScript_DIO/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Carlos", 37));
+console.log(mostraCidade("Carlos", "Cabo de Santo Agostinho"));
+console.log(mostraHobby("Carlos", "Jogar video gamer"));
