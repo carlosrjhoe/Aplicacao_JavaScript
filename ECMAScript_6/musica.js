4ca4871a0f231bccae838a8932f8a9e262f4293b
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-18 19:17:06 -0300

    exercicios

--- ECMAScript_6/musica.js
+++ ECMAScript_6/musica.js
@@ -6,8 +6,20 @@ for(var musica of musicas){
   console.log(musica);
 }
 
-var musicas1 = new Set(['musica1','musica2 ','musica3']);
+var musicas1 = new Set(['musica1','musica2','musica3']);
 musicas1.delete('musica2');
+// musicas1.clear();
 for(var musi of musicas1){
-  console.log(musi);
+  if(musicas1.has('musica4')){
+    console.log('Já existe na lista');
+    break;
+  } else {
+    console.log('Não existe na lista.');
+    break;
+  }
 }
+
+var qtdmusicas1 = musicas1.size;
+var qtdmusicas = musicas.size;
+console.log(`Há ${qtdmusicas} de musicas na lista!`);
+console.log(`Há ${qtdmusicas1} de musicas na lista!`);
