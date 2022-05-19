b8157be89506d4949a7571a9442af3c5ac6b86d5
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-19 05:22:50 -0300

    exercicios

---
+++ ECMAScript_6/var.js
@@ -0,0 +1,10 @@
+var objeto = {};
+var numero = 1;
+var nome = "Carlos";
+var lista = [1, 2, 3];
+
+console.log(typeof(objeto));
+console.log(typeof(numero));
+console.log(typeof(nome));
+console.log(typeof(lista));
+
