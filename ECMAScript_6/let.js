9da97a776109fee3bf11a5d4bc914930f3971bb0
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-19 05:22:30 -0300

    exercicios

---
+++ ECMAScript_6/let.js
@@ -0,0 +1,17 @@
+function soma(a, b){
+  return a + b;
+}
+let resultado = 0;
+resultado = soma(2, 3);
+console.log(`Soma = ${resultado}`);
+
+var id = 2;
+var id = 1;
+// const id = 3;
+console.log(id);
+
+var msg = 'Olá';
+function msg(){
+  var msg = 'Adeus';
+}
+console.log(msg);
