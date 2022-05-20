68c8163a91851788e8d5bdedad52924128d24f83
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-20 04:51:00 -0300

    Exercicios

---
+++ Udemy_Curso_JavaScript/funcional/first-class-function.js
@@ -0,0 +1,19 @@
+const soma = function(a, b){
+  return a + b;
+}
+
+function subtrair (a, b){
+  let resultado = a - b
+  return resultado;
+}
+
+const mutiplicar = (a, b) => {
+  return a * b;
+}
+
+const dividir = (a, b) => a / b;
+
+console.log(soma(10, 5));
+console.log(subtrair(10, 5));
+console.log(mutiplicar(10, 5));
+console.log(dividir(10, 5));
