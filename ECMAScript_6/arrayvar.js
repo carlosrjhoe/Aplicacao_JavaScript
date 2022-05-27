954c89091531b6b5f17b01784194a04efadde3dc
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-27 15:53:00 -0300

    Exercicios

---
+++ ECMAScript_6/arrayvar.js
@@ -0,0 +1,19 @@
+// const arrayVar = [];
+// for (var i = 1; i < 5; i++){
+//   arrayVar.push(function(){
+//     console.log(i)
+//   })
+// }
+
+// const arrayVar1 = [];
+// for (let i = 1; i < 5; i++){
+//   arrayVar.push(function(){
+//     console.log(i)
+//   })
+
+
+function imprimeNome(nome){
+  console.log(nome);
+}
+
+imprimeNome('Carlos Roberto');
