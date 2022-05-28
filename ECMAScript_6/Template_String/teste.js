5ebf010be45b77962b327cad253f53c852300709
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-28 14:40:57 -0300

    Adicionando o typeScript e incrementando uma forma de desenvolver

---
+++ ECMAScript_6/Template_String/teste.js
@@ -0,0 +1,12 @@
+const login = 'EcmaScript';
+const dia = '27 de Maio';
+const ano = 2021;
+
+console.log(`Olá, ${login}, \nHoje é ${dia} de ${ano}`);
+
+console.log(`
+Carlos
+Mayara
+Neto
+Luna
+`)
--- Node_JS_DIO/Rest_NodeJS/Live-01/src/index.ts
+++ Node_JS_DIO/Rest_NodeJS/Live-01/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/status', (req, res, next) => {
-  res.status(200).send('Olá Mundo!')
+  res.status(200).send('Olá Mundo!, Vamos começar a aprogramar...')
 });
 
 app.listen(3000, () => {
