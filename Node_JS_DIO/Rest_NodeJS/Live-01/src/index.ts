8cef2541d82e20bff3e92ed1bdcdbbe1412c9c07
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-28 13:42:39 -0300

    Criando um servidor express

--- Node_JS_DIO/Rest_NodeJS/Live-01/src/index.ts
+++ Node_JS_DIO/Rest_NodeJS/Live-01/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Executei!');
+import express from 'express';
+const app = express();
+
+app.get('/status', (req, res, next) => {
+  res.status(200).send('Olá Mundo!')
+});
+
+app.listen(3000, () => {
+  console.log('Aplicação rodando na porta 3000, Executei!');
+});
