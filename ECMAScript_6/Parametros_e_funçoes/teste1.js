d167197992cfa7874242f4782de5563d724a46a1
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-29 14:22:17 -0300

    Testando parametros e funções

---
+++ ECMAScript_6/Parametros_e_funçoes/teste1.js
@@ -0,0 +1,13 @@
+function mostraNome(nome){
+  console.log(nome);
+}
+
+mostraNome('Carlos');
+mostraNome();
+
+function nomeSobreNome(nome, sobrenome, ultimoNome){
+  console.log(nome, sobrenome, ultimoNome);
+}
+
+nomeSobreNome();
+nomeSobreNome('Mayara', 'Ramos');
