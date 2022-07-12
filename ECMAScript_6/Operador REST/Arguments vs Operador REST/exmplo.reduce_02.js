2184f267a5b6ab1b2d7cd4c0d8c2d93519a478b3
Carlos Roberto <carlosrjhoe@gmail.com>
2022-07-12 12:49:39 -0300

    Estudando Reduce

--- ECMAScript_6/Operador REST/Arguments vs Operador REST/exmplo.reduce_02.js
+++ ECMAScript_6/Operador REST/Arguments vs Operador REST/exmplo.reduce_02.js
@@ -1,12 +1,29 @@
-function somar (...valores) {
-  let soma = 0;
-  const qtd = valores.length;
-  for(let i =0; i < qtd; i++) {
-    soma += valores[i];
-  }
-  return soma
+// function somar (...valores) {
+//   let soma = 0;
+//   const qtd = valores.length;
+//   for(let i =0; i < qtd; i++) {
+//     soma += valores[i];
+//   }
+//   return soma
+// }
+
+// console.log(`A soma é = ${somar(1, 2, 3, 4, 5, 2)}`)
+// console.log(`A soma é = ${somar(5, 10)}`)
+// console.log(`A soma é = ${somar(3, 8)}`)
+
+// 2º Forma de trabalhar com ARGUMENTS	X	OPERADOR	REST
+
+function somar(...valores) {
+  return valores.reduce((soma, valor) => {
+    return soma + valor;
+  }, 0)
 }
 
-console.log(`A soma é = ${somar(1, 2, 3, 4, 5, 2)}`)
-console.log(`A soma é = ${somar(5, 10)}`)
-console.log(`A soma é = ${somar(3, 8)}`)
+function mult(...valores) {
+  return valores.reduce((produto, valor) =>{
+    return produto * valor
+  }, 1)
+}
+
+console.log(`A soma = ${somar(1, 2)}`)
+console.log(`A soma = ${mult(3, 5)}`)
